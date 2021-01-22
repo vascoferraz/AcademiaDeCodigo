eb9fad08c3497f1c2ca8fffc11ac34035e7e24a0
VascoFerraz <vascoferraz@gmail.com>
2021-01-22 00:05:35 +0000

    First working version: GuessNumber

--- GuessNumber/src/org/academiadecodigo/bootcamp56/guessnumber/Game.java
+++ GuessNumber/src/org/academiadecodigo/bootcamp56/guessnumber/Game.java
@@ -12,19 +12,41 @@ public class Game {
 
         randomSecret = Random.random(number);
 
-        //System.out.println("Random secret: " + randomSecret);
-        //System.out.println("Player 1: " + value1);
-        //System.out.println("Player 2: " + value2);
+        System.out.println("Random secret: " + randomSecret);
 
-        int value1 = player1.getNumber(number);
-        if (numberIsCorrect(value1) == true) {
-            System.out.println("Player 1 wins");
-        }
+        int value1 = 0;
+        int value2 = 0;
 
-        int value2 = player2.getNumber(number);
-        if (numberIsCorrect(value2) == true) {
-            System.out.println("Player 2 wins");
-        }
+          while (value1 != randomSecret && value2 != randomSecret) {
+
+              value1 = player1.getNumber(number);
+              value2 = player2.getNumber(number);
+
+              if (numberIsCorrect(value1) == true) {
+                  System.out.println("Player 1: " + value1);
+                  System.out.println("Player 1: Wins");
+                  break;
+              }
+
+              if (numberIsCorrect(value1) == false) {
+                  System.out.println("Player 1: " + value1);
+                  System.out.println("Player 1: Incorrect guess");
+              }
+
+              if (numberIsCorrect(value2) == true) {
+                  System.out.println("Player 2: " + value2);
+                  System.out.println("Player 2: Wins");
+                  break;
+              }
+
+              if (numberIsCorrect(value2) == false) {
+                  System.out.println("Player 2: " + value2);
+                  System.out.println("Player 2: Incorrect guess");
+              }
+
+          }
+
+
 
     }
 
--- GuessNumber/src/org/academiadecodigo/bootcamp56/guessnumber/Main.java
+++ GuessNumber/src/org/academiadecodigo/bootcamp56/guessnumber/Main.java
@@ -3,9 +3,6 @@ package org.academiadecodigo.bootcamp56.guessnumber;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        //Game game = new Game();
-        //game.start(10);
 
         Game game = new Game(10);
 
