8a4eb7d97d3dfa21849f74b66387c65af8908f68
VascoFerraz <vascoferraz@gmail.com>
2021-01-23 20:44:37 +0000

    First working version: RockPaperScissors

--- rockpaperscissors/src/org/academiadecodigo/bootcamp56/rockpaperscissors/Game.java
+++ rockpaperscissors/src/org/academiadecodigo/bootcamp56/rockpaperscissors/Game.java
@@ -9,76 +9,112 @@ public class Game {
     // PAPER(2),   // Code 2
     // SCISSOR(3); // Code 3
 
-    public Game() {
+    public Game(int numberOfGames) {
 
+        int player1Choice = 0;
+        int player2Choice = 0;
 
-        int player1Choice = player1.getType();
-        int player2Choice = player2.getType();
+        int numberOfWinsPlayer1 = 0;
+        int numberOfWinsPlayer2 = 0;
 
         Random[] type = Random.values();
 
-        boolean gameIsFinished = false;
+        System.out.println("-----------------------------");
 
-        while (!gameIsFinished == true) {
+        while (numberOfGames > 0) {
 
+                player1Choice = player1.getType();
+                player2Choice = player2.getType();
 
             if (player1Choice == 1 && player2Choice == 2) {
-                System.out.println("Player 1: " + type[player1Choice-1] + "   | Code: " + player1Choice);
+                System.out.println("Player 1: " + type[player1Choice-1] + "  | Code: " + player1Choice);
                 System.out.println("Player 2: " + type[player2Choice-1] + " | Code: " + player2Choice);
                 System.out.println("Player 2: Wins");
-                gameIsFinished = true;
-                break;
+                System.out.println("-----------------------------");
+                numberOfGames--;
+                numberOfWinsPlayer2++;
+                continue;
             }
 
             if (player1Choice == 1 && player2Choice == 3) {
                 System.out.println("Player 1: " + type[player1Choice-1] + "    | Code: " + player1Choice);
                 System.out.println("Player 2: " + type[player2Choice-1] + " | Code: " + player2Choice);
                 System.out.println("Player 1: Wins");
-                gameIsFinished = true;
-                break;
+                System.out.println("-----------------------------");
+                numberOfGames--;
+                numberOfWinsPlayer1++;
+                continue;
             }
 
             if (player1Choice == 2 && player2Choice == 1) {
                 System.out.println("Player 1: " + type[player1Choice-1] + " | Code: " + player1Choice);
                 System.out.println("Player 2: " + type[player2Choice-1] + "  | Code: " + player2Choice);
                 System.out.println("Player 1: Wins");
-                gameIsFinished = true;
-                break;
+                System.out.println("-----------------------------");
+                numberOfGames--;
+                numberOfWinsPlayer1++;
+                continue;
             }
 
             if (player1Choice == 2 && player2Choice == 3) {
                 System.out.println("Player 1: " + type[player1Choice-1] + "   | Code: " + player1Choice);
                 System.out.println("Player 2: " + type[player2Choice-1] + " | Code: " + player2Choice);
                 System.out.println("Player 2: Wins");
-                gameIsFinished = true;
-                break;
+                System.out.println("-----------------------------");
+                numberOfGames--;
+                numberOfWinsPlayer2++;
+                continue;
             }
 
             if (player1Choice == 3 && player2Choice == 1) {
                 System.out.println("Player 1: " + type[player1Choice-1] + " | Code: " + player1Choice);
                 System.out.println("Player 2: " + type[player2Choice-1] + "    | Code: " + player2Choice);
                 System.out.println("Player 2: Wins");
-                gameIsFinished = true;
-                break;
+                System.out.println("-----------------------------");
+                numberOfGames--;
+                numberOfWinsPlayer2++;
+                continue;
             }
 
             if (player1Choice == 3 && player2Choice == 2) {
                 System.out.println("Player 1: " + type[player1Choice-1] + " | Code: " + player1Choice);
                 System.out.println("Player 2: " + type[player2Choice-1] + "   | Code: " + player2Choice);
                 System.out.println("Player 1: Wins");
-                gameIsFinished = true;
-                break;
+                System.out.println("-----------------------------");
+                numberOfGames--;
+                numberOfWinsPlayer1++;
+                continue;
             }
 
             else {
                 System.out.println("Player 1: " + type[player1Choice-1] + " | Code: " + player1Choice);
                 System.out.println("Player 2: " + type[player2Choice-1] + " | Code: " + player2Choice);
                 System.out.println("Tie");
-                gameIsFinished = true;
-                break;
+                System.out.println("-----------------------------");
+                numberOfGames--;
+                continue;
             }
 
         }
+
+        System.out.println("Player 1 wins: " + numberOfWinsPlayer1);
+        System.out.println("Player 2 wins: " + numberOfWinsPlayer2);
+
+        if (numberOfWinsPlayer1 > numberOfWinsPlayer2) {
+            System.out.println("Player 1 is the winner");
+        }
+
+        if (numberOfWinsPlayer1 < numberOfWinsPlayer2) {
+            System.out.println("Player 2 is the winner");
+        }
+
+        if (numberOfWinsPlayer1 == numberOfWinsPlayer2) {
+            System.out.println("Tie");
+        }
+
+        System.out.println("-----------------------------");
+
+
 
     }
 
--- rockpaperscissors/src/org/academiadecodigo/bootcamp56/rockpaperscissors/Random.java
+++ rockpaperscissors/src/org/academiadecodigo/bootcamp56/rockpaperscissors/Random.java
@@ -18,5 +18,4 @@ public enum Random {
         return type;
     }
 
-
 }
