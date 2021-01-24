fe1ff4c46cb61b80a5b161e3210f6eb496901b3a
vascoferraz <vascoferraz@hotmail.com>
2021-01-24 18:20:02 +0000

    Bugs corrections: searchAndSort

--- otherProjects/searchAndSort/insertionSort.js
+++ otherProjects/searchAndSort/insertionSort.js
@@ -1,31 +1,23 @@
 var unsortedArray = [2, 0, 9, 3, 4, 7, 1, 5];
 
-function insertionSort(array)
-{
-  var i=0;
-  var j=0;
-  var temp=0;
-  console.log(array.join(' - '));
-  
-  for (i=0 ; i<array.length ; i++) 
-  {  
-   
-    for (j=0 ; j<array.length ; j++)
-    {
-  
-     if (array[i] < array[j])
-     //swap
-     {
-      temp = array[j];
-      array[j] = array[i];
-      array[i] = temp;    
-     }   
-      
-    }
-    console.log(array);
-  }
-    
-}
+function insertionSort(array) {
 
+	var i=0;	
+	
+	for (i=0 ; i<array.length ; i++)
+	{  
+	  if (array[i+1] < array[i])
+	  {
+	    var temp = array[i];
+	    array[i] = array[i+1];
+	    array[i+1] = temp;
+	    i=0;
+	  }  	 
+	 
+	  console.log(array); 
+	}
+
+}
+  
 insertionSort(unsortedArray);
 
--- otherProjects/searchAndSort/selectionSort.js
+++ otherProjects/searchAndSort/selectionSort.js
@@ -1,31 +1,31 @@
-var unsortedArray = [9, 1, 3, 2, 4, 5, 7, 6, 77, 23, 11, 15];
+var unsortedArray = [9, 1, 3, 2, 4, 5, 7, 6, 77, 99, 11, 15];
 
-function selectionSort(array)
-{
-  var i=0;
-  var j=0;
-  var temp=0;
-  console.log(array.join(' - '));
-  
-  for (i=0 ; i<array.length ; i++) 
-  {  
-   
-    for (j=0 ; j<array.length ; j++)
-    {
-  
-     if (array[i] < array[j])
-     //swap
-     {
-      temp = array[j];
-      array[j] = array[i];
-      array[i] = temp;    
-     }   
-      
-    }
+function selectionSort(array) {
+
+    let n = array.length;
     console.log(array);
-  }
+        
+    for(let i = 0; i < n; i++) {
+        // Finding the smallest number in the subarray
+        let min = i;
+        
+        for(let j = i+1; j < n; j++){
+            
+            if(array[j] < array[min]) {
+                min=j;
+            }
+         }
+         
+         if (min != i) {
+             // Swapping the elements
+             let tmp = array[i]; 
+             array[i] = array[min];
+             array[min] = tmp;      
+        }
+        
+        console.log(array);
+    }
     
 }
 
-insertionSort(unsortedArray);
-
+selectionSort(unsortedArray);
