f933e28a007cadd9ec9a33cf181a6ed3a59fe9e7
vascoferraz <vascoferraz@hotmail.com>
2021-02-16 23:17:10 +0000

    homework

---
+++ homework/src/org/academiadecodigo/asynctomatics/homework/Reference.java
@@ -0,0 +1,24 @@
+package org.academiadecodigo.asynctomatics.homework;
+
+public class Reference {
+
+    public static void main(String[] args) {
+        Mutant mutant = new Mutant();
+
+        mutant.name = "Magneto";
+        System.out.println("Inside main(): " + mutant.name + mutant);
+
+        transformIntoWolverine(mutant);
+        System.out.println("Inside main(): " + mutant.name + mutant);
+    }
+
+        static void transformIntoWolverine(Mutant wolverine) {
+            System.out.println("Inside method() :" + wolverine.name + wolverine);
+            wolverine.name = "Wolverine";
+            System.out.println("Inside method() :" + wolverine.name + wolverine);
+        }
+    }
+
+    class Mutant {
+        String name;
+    }
