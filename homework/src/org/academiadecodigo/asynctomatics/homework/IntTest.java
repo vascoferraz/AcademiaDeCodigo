c3983537ef0bbe707fd10a454b3eb8184eedc8d5
vascoferraz <vascoferraz@hotmail.com>
2021-02-17 20:31:47 +0000

    homework - v3

--- homework/src/org/academiadecodigo/asynctomatics/homework/IntTest.java
+++ homework/src/org/academiadecodigo/asynctomatics/homework/IntTest.java
@@ -3,7 +3,7 @@ package org.academiadecodigo.asynctomatics.homework;
 public class IntegerTest2 {
 
     public static void main(String[] args) {
-        MyInteger myNumber = new MyInteger();
+        MyInt myNumber = new MyInt();
 
         myNumber.number = 10;
         System.out.println("Inside main(): " + myNumber.number + " | " + myNumber);
@@ -12,13 +12,13 @@ public class IntegerTest2 {
         System.out.println("Inside main(): " + myNumber.number + " | " + myNumber);
     }
 
-    static void transformIntoWolverine(MyInteger otherNumber) {
+    static void transformIntoWolverine(MyInt otherNumber) {
         System.out.println("Inside method() :" + otherNumber.number + " | " + otherNumber);
         otherNumber.number = 20;
         System.out.println("Inside method() :" + otherNumber.number + " | " + otherNumber);
     }
 }
 
-class MyInteger {
-    Integer number;
+class MyInt {
+    int number;
 }
