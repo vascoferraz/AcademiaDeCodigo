d85ee39333d9895d174dc2ce0a4146becdd787e8
vascoferraz <vascoferraz@hotmail.com>
2021-02-25 23:43:11 +0000

    First (beta) working version: ChatV2 using threads

--- chatwiththreadsv2/src/org/academiadecodigo/asynctomatics/chatwiththreadsv2/ClientHandler.java
+++ chatwiththreadsv2/src/org/academiadecodigo/asynctomatics/chatwiththreadsv2/ClientHandler.java
@@ -2,17 +2,22 @@ package org.academiadecodigo.asynctomatics.chatwiththreadsv2;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.LinkedList;
+import java.util.Scanner;
 
-public class ClientHandler extends Thread{
+public class ClientHandler implements Runnable {
 
-    private final Socket clientSocket;
+    private Socket clientSocket;
+    private LinkedList<Socket> myList;
     private InputStream inputStream;
     private OutputStream outputStream;
+
     private String message = "";
+    private String userName;
 
-    public ClientHandler(Socket clientSocket) throws IOException {
-
+    public ClientHandler(Socket clientSocket, LinkedList myList) {
         this.clientSocket = clientSocket;
+        this.myList = myList;
     }
 
     @Override
@@ -27,25 +32,44 @@ public class ClientHandler extends Thread{
     }
 
     public void handleClientSocket() throws IOException, InterruptedException {
+
         inputStream = clientSocket.getInputStream();
         outputStream = clientSocket.getOutputStream();
 
+        setUserName();
+
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         String line;
-        while ( (line = reader.readLine()) != null) {
+        while ((line = reader.readLine()) != null) {
             if ("quit".equalsIgnoreCase(line)) {
                 break;
             }
 
-            String message = "You typed: " + line + "\n";
-            outputStream.write(message.getBytes());
+            String message = userName + ": " + line + "\n";
+
+            for (int i = 0; i < myList.size(); i++) {
+
+                if (myList.get(i) == this.clientSocket) {
+                    continue;
+                }
+                myList.get(i).getOutputStream().write(message.getBytes());
+                outputStream.flush();
+            }
         }
+
 
         clientSocket.close();
     }
 
-    public void sendMessage() throws IOException {
-        outputStream.write(message.getBytes());
-    }
+    private void setUserName() throws IOException {
 
+        outputStream.write("Your name is: ".getBytes());
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        userName = reader.readLine();
+
+        outputStream.write(("Welcome: " + userName + "\n").getBytes());
+        outputStream.flush();
+    }
 }
+
--- chatwiththreadsv2/src/org/academiadecodigo/asynctomatics/chatwiththreadsv2/Server.java
+++ chatwiththreadsv2/src/org/academiadecodigo/asynctomatics/chatwiththreadsv2/Server.java
@@ -4,28 +4,36 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException {
 
         int port = 10201;
+        int numberOfThreads = 10;
         ServerSocket serverSocket = new ServerSocket(port);
 
-        LinkedList myList = new LinkedList();
+        LinkedList<Socket> myList = new LinkedList();
+
+        ExecutorService fixedPool = Executors.newFixedThreadPool(numberOfThreads);
+
 
         while (true) {
             Socket clientSocket = serverSocket.accept();
             System.out.println("Accepted connection from " + clientSocket);
-            myList.add(new ClientHandler(clientSocket));
-            ClientHandler clientHandler = (ClientHandler) (myList.getLast());
-            clientHandler.start();
-            System.out.println(myList.size());
-            clientHandler.handleClientSocket();
-
+            myList.add(clientSocket);
+            fixedPool.submit((new ClientHandler(clientSocket, myList)));
         }
     }
+
 }
+
+
+
+
 
 
 
